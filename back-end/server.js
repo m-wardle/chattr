323ccf39813ccaac54733f81760a508c19cd861a
Matt Wardle <mwardle.22@gmail.com>
2020-02-04 11:26:36 -0500

    Install MongoDB and begin back-end setup

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,19 @@
 const WebSocket = require('ws');
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/chattr";
 
 const wss = new WebSocket.Server({ port: 3001 });
 
+MongoClient.connect(url, (err, db) => {
+  if (err) throw err;
+  console.log("Database Created.");
+  db.close;
+})
+
 wss.on('connection', (ws) => {
+  console.log("User connected;")
   ws.on('message', (data) => {
+    console.log(`${JSON.parse(data).name} said: ${JSON.parse(data).message}`)
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState == WebSocket.OPEN) {
         client.send(data);
